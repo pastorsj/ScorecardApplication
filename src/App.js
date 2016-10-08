04fc5c95f5846b5d22acf873620c183069f44568
Sam Pastoriza <pastorsj@rose-hulman.edu>
2016-10-07 22:28:50 -0400

    Updated to semantic

--- src/App.js
+++ src/App.js
@@ -10,40 +10,24 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-default navbar-fixed-top navbar-color-on-scroll" role="navigation">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <Link to="/">
-                <img src={logo} className="navbar-brand logo" alt="logo"/>
-              </Link>
-            </div>
-            <div className="collapse navbar-collapse navbar-left">
-              <ul className="nav navbar-nav">
-                <li>
-                  <button type="button" className="btn rose-bg" onClick={this.nav.bind(this, '/round')}>
-                    Add A Round
-                  </button>
-                </li>
-                <li>
-                  <button type="button" className="btn rose-bg" onClick={this.nav.bind(this, '/rounds')}>
-                    View Recent Rounds
-                  </button>
-                </li>
-                <li>
-                  <button type="button" className="btn rose-bg" onClick={this.nav.bind(this, '/statistics')}>
-                    View Statistics
-                  </button>
-                </li>
-              </ul>
-            </div>
-            <div className="collapse navbar-collapse navbar-right">
-              <ul className="nav navbar-nav">
-                <li><button type="button" className= "btn btn-success">Logout</button></li>
-              </ul>
-            </div>
+        <div className="ui secondary pointing menu">
+          <a className="item" onClick={this.nav.bind(this, '/round')}>
+            Add a Round
+          </a>
+          <a className="item" onClick={this.nav.bind(this, '/rounds')}>
+            View Recent Rounds
+            Messages
+          </a>
+          <a className="item" onClick={this.nav.bind(this, '/statistics')}>
+            View Statistics
+          </a>
+          <div className="right menu">
+            <a className="ui item">
+              Logout
+            </a>
           </div>
-        </nav>
-        <div>
+        </div>
+        <div className="ui segment">
           {this.props.children}
         </div>
       </div>
